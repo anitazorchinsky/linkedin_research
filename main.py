10a5646e741586bf0094b803c45210e6bf167888
anitazorchinsky <zorchinsky2014@gmail.com>
2025-01-12 18:51:54 +0200

    chore: remove unused functions & code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 import urllib.parse
 import uuid
@@ -15,59 +13,17 @@ while user_answer == "yes":
    user_answer = input("enter yes or no: ")
 
 
-MAX_VACANCY = int(input("How many vacancies do you want? ")) // 10
-options = webdriver.ChromeOptions()
-options.add_argument("--incognito")
-driver = webdriver.Chrome(options=options)
-
-def generate_url(currentJobId, keywords, origin, refresh,position, pageNum,start, geoId=101620260):
-    base_link = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?"
-    return f"{base_link}&geoId={geoId}&keywords={keywords}&origin={origin}&refresh={refresh}&position={position}&pageNum={pageNum}&start={start}"
-
-
-def login():
-    login_email = "cabil42488@myweblaw.com"
-    login_password = "3$&Kq6pmuQaEA39"
-    url_login = "https://www.linkedin.com/uas/login"
-    driver.get(url_login)
-    driver.find_element(By.ID,"username").send_keys(login_email)
-    driver.find_element(By.ID,"password").send_keys(login_password)
-    driver.find_element(By.TAG_NAME,"button").click()
-
-def get_soup_by_link(link):
-    driver.get(link)
-    time.sleep(5)
-    scroll_pause_time = 2
-    screen_height = driver.execute_script("return window.screen.height;")
-    i = 1
-    while True:
-        driver.execute_script(f"window.scrollTo(0, {screen_height * i});")
-        i += 1
-        time.sleep(scroll_pause_time)
-        scroll_height = driver.execute_script("return document.body.scrollHeight;")
-        if screen_height * i > scroll_height:
-            break
-    source = driver.page_source
-    return BeautifulSoup(source, "html.parser")
-
-def get_company_name_by_link(soup):
-    company_name = None
-    company_name = soup.find("a", class_="topcard__org-name-link topcard__flavor--black-link")
-    if company_name is None:
-        company_name = None
-    else:
-        company_name = company_name.text.strip()
-    return company_name
+MAX_VACANCY = int(input("How many vacancies do you want? "))
 
 
 l = []
 target_url='https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?{}&location=Tel%20Aviv&geoId=101620260&start={}'
 for keyword in keywords:
-    for i in range(MAX_VACANCY):
+    for i in range(0,MAX_VACANCY,10):
         time.sleep(2)
         res = requests.get(target_url.format(urllib.parse.urlencode({"keywords": keyword }),i))
-        soup=BeautifulSoup(res.text,'html.parser')
-        alljobs_on_this_page=soup.find_all("li")
+        soup = BeautifulSoup(res.text,'html.parser')
+        alljobs_on_this_page = soup.find_all("li")
         for x in range(0,len(alljobs_on_this_page)):
             jobid = alljobs_on_this_page[x].find("div",{"class":"base-card"}).get('data-entity-urn').split(":")[3]
             l.append(jobid)
@@ -78,9 +34,10 @@ jobs_table = {"vacancy_title":[],"company_title":[],"description":[],"seniority_
 l = list(set(l))
 for id in l:
     time.sleep(2)
-    job_link = "https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/"+id
+    print(f"{l.index(id) + 1} / {len(l)}")
+    job_link = "https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/" + id
     res = requests.get(job_link)
-    soup=BeautifulSoup(res.text,'html.parser')
+    soup = BeautifulSoup(res.text,'html.parser')
     try:
         vacancy_title = soup.find("h2",{"class":"top-card-layout__title"}).text
     except:
@@ -115,5 +72,3 @@ for id in l:
 
 df = pd.DataFrame(jobs_table)
 df.to_excel(f"{uuid.uuid4()}.xlsx")
-
-driver.close()
